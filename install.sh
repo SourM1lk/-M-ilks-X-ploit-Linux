0a37be9f0a284f40598fcdda23bf25a67b354314
SourM1lk <rottenm1lk@proton.me>
2023-09-21 15:25:41 -0600

    ohmyzsh fail, comment for now

--- install.sh
+++ install.sh
@@ -26,22 +26,22 @@ echo "Setting Zsh as the default shell..."
 chsh -s $(which zsh)
 
 # Oh My Zsh
-echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+#echo "Installing Oh My Zsh..."
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Cloning Zsh plugins
-echo "Cloning Zsh plugins..."
-plugins_directory=~/.oh-my-zsh/custom/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $plugins_directory/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $plugins_directory/zsh-syntax-highlighting
+#echo "Cloning Zsh plugins..."
+#plugins_directory=~/.oh-my-zsh/custom/plugins
+#git clone https://github.com/zsh-users/zsh-autosuggestions.git $plugins_directory/zsh-autosuggestions
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $plugins_directory/zsh-syntax-highlighting
 
 # Editing ~/.zshrc to include the plugins
-echo "Editing ~/.zshrc to include the plugins..."
-sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+#echo "Editing ~/.zshrc to include the plugins..."
+#sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 
 # Activating the changes
-echo "Activating the changes..."
-source ~/.zshrc
+#echo "Activating the changes..."
+#source ~/.zshrc
 
 # Installing Tmux
 echo "Installing Tmux..."
