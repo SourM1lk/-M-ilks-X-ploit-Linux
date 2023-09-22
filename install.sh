fa9f39a9c3502b9ae82bde740d906c48d0002df6
SourM1lk <rottenm1lk@proton.me>
2023-09-22 08:33:11 -0600

    brave fix

--- install.sh
+++ install.sh
@@ -139,7 +139,7 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 
 # Add Brave browser repository to sources.list.d
 echo "Adding Brave browser repository..."
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 # Update the package list to include the Brave browser repository
 echo "Updating package list..."
